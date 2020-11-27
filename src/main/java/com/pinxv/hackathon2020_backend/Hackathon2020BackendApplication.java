74554647fc77ba67cf54bef0d517339bcc3c37cd
fengguohao <58283120+fengguohao@users.noreply.github.com>
2020-11-27 21:11:06 +0800

    init

---
+++ src/main/java/com/pinxv/hackathon2020_backend/Hackathon2020BackendApplication.java
@@ -0,0 +1,13 @@
+package com.pinxv.hackathon2020_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2020BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon2020BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinxv/hackathon2020_backend/Hackathon2020BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinxv.hackathon2020_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2020BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
