8fb06e565a5293c3e5a76112460447bfd7469f82
fengguohao <58283120+fengguohao@users.noreply.github.com>
2020-11-28 14:59:53 +0800

    feat: 获取新闻列表

---
+++ src/main/java/com/pinxv/hackathon2020_backend/selenium/NewsCrawler.java
@@ -0,0 +1,38 @@
+package com.pinxv.hackathon2020_backend.selenium;
+
+import cn.hutool.core.util.URLUtil;
+import com.pinxv.hackathon2020_backend.vo.NewsVO;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>description: </p>
+ *
+ * @date 2020/11/28
+ */
+public class NewsCrawler extends Crawler{
+    private static String urlbase = "https://www.baidu.com/s?tn=news&word=";
+
+    public List<NewsVO> crawl(String keyword){
+        List<NewsVO> newsVOS = new ArrayList<>();
+        String realURL = urlbase+URLUtil.encode(keyword);
+        DRIVER.get(realURL);
+        List<WebElement> newsDescription = DRIVER.findElements(By.xpath("//div[@class='result-op c-container xpath-log new-pmd']//span[@class='c-font-normal c-color-text']"));
+        List<WebElement> newsTitle = DRIVER.findElements(By.xpath("//div[@class='result-op c-container xpath-log new-pmd']//h3//a"));
+        for(int i = 0;i<newsDescription.size();i++){
+            NewsVO newsVO = new NewsVO();
+            newsVO.setTitle(newsTitle.get(i).getText());
+            newsVO.setUrl(newsTitle.get(i).getAttribute("href"));
+            newsVO.setDescription(newsDescription.get(i).getText());
+            newsVOS.add(newsVO);
+        }
+        return newsVOS;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
